1262184c934cb41d7da1b9db6e92ba58e3beb2e6
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-22 17:25:39 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,12 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# def roll
+#   return rand(1..6)
+# end
+
+
 def roll
-  # code goes here
+  nums = [1,2,3,4,5,6]
+  return nums.sample
 end
